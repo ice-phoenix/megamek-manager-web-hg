39399fa9f6b129ff76e1a0f61598fb499c4ff787
Marat Kh. Akhin <Marat.Akhin@gmail.com>
2013-05-06 23:32:34 +0400

    Updated project build description

--- project/Build.scala
+++ project/Build.scala
@@ -4,18 +4,19 @@ import play.Project._
 
 object ApplicationBuild extends Build {
 
-  val appName         = "megamek-manager-web"
-  val appVersion      = "1.0-SNAPSHOT"
+  val appName = "megamek-manager-web"
+  val appVersion = "0.1-SNAPSHOT"
 
   val appDependencies = Seq(
     // Add your project dependencies here,
     jdbc,
-    anorm
+    anorm,
+    "info.ice-phoenix" % "megamek-manager" % "0.1-SNAPSHOT"
   )
 
-
   val main = play.Project(appName, appVersion, appDependencies).settings(
-    // Add your own project settings here      
+    // Add your own project settings here
+    resolvers += "Local Maven Repository" at "file://" + Path.userHome.absolutePath + "/.m2/repository"
   )
 
 }
