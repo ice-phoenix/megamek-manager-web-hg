7ba4acf9e3f5a8949881d54027f0cc838d40b7df
Marat Kh. Akhin <Marat.Akhin@gmail.com>
2013-05-18 19:02:44 +0400

    m

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,7 +2,8 @@ package controllers
 
 import play.api.mvc._
 
-object Application extends Controller {
+object Application
+  extends Controller {
 
   def index = Action {
     Ok(views.html.main.render())
---
+++ app/controllers/mmm/Admin.scala
@@ -0,0 +1,8 @@
+package controllers.mmm
+
+import play.api.mvc.Controller
+
+class Admin
+  extends Controller {
+  // TODO: akhin Implement =)
+}
