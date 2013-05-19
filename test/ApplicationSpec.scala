c95deb136533747b527ad0b78071cdad109f31a0
Marat Kh. Akhin <Marat.Akhin@gmail.com>
2013-05-19 22:21:23 +0400

    m

--- test/ApplicationSpec.scala
+++ test/ApplicationSpec.scala
@@ -1,9 +1,8 @@
 package test
 
 import org.specs2.mutable._
-
-import play.api.test._
 import play.api.test.Helpers._
+import play.api.test._
 
 /**
  * Add your spec here.
@@ -11,23 +10,25 @@ import play.api.test.Helpers._
  * For more information, consult the wiki.
  */
 class ApplicationSpec extends Specification {
-  
+
   "Application" should {
-    
+
     "send 404 on a bad request" in {
       running(FakeApplication()) {
-        route(FakeRequest(GET, "/boum")) must beNone        
+        route(FakeRequest(GET, "/boum")) must beNone
       }
     }
-    
+
     "render the index page" in {
       running(FakeApplication()) {
         val home = route(FakeRequest(GET, "/")).get
-        
+
         status(home) must equalTo(OK)
         contentType(home) must beSome.which(_ == "text/html")
-        contentAsString(home) must contain ("Your new application is ready.")
+        contentAsString(home) must contain("MegaMek Manager")
       }
     }
+
   }
+
 }
