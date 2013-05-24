54549617ec874dbfa0f41632dd267039f99c6b5f
Marat Kh. Akhin <Marat.Akhin@gmail.com>
2013-05-24 21:49:03 +0400

    editOnClick implemented

--- public/js/mmm/admin-config-list.js
+++ public/js/mmm/admin-config-list.js
@@ -61,31 +61,78 @@ angular.module('mmm.adminconfiglist', ['ui.bootstrap',
   $scope.getConfig();
 }])
 
-.directive('editOnClick', ['$timeout', function($timeout) {
+.directive('onKeyUp', ['$parse', function($parse) {
+  return {
+    restrict: 'A',
+    replace: false,
+    transclude: false,
+    scope: true,
+    link: function(scope, element, attrs) {
+      var callback = $parse(attrs.onKeyUp);
+      element.bind('keyup', function(event) {
+        scope.$apply(function() {
+          callback(scope, {$event:event});
+        });
+      });
+    }
+  };
+}])
+
+.directive('editOnClick', ['$parse', '$timeout', function($parse, $timeout) {
   return {
     restrict: 'E',
     replace: true,
     transclude: false,
-    scope: {
-      get: '&',
-      set: '&'
-    },
+    scope: true,
     template: '<div>' +
-                '<span ng-switch="isEditMode">' +
-                  '<input type="text" class="input-compact" ng-switch-when="false" ng-model="editValue" readonly ng-click="enterEditMode()"></input>' +
-                  '<input type="text" class="input-compact" ng-switch-when="true"  ng-model="editValue"                                    ></input>' +
+                '<span ng-switch="model.isEditMode">' +
+                  '<input type="text" class="input-compact" ng-switch-when="false" ng-model="model.editValue" readonly ng-click="enterEditMode($event)"></input>' +
+                  '<input type="text" class="input-compact" ng-switch-when="true"  ng-model="model.editValue"          on-key-up="exitEditMode($event)"></input>' +
                 '</span>' +
               '</div>',
     link: function(scope, element, attrs) {
 
-      scope.isEditMode = false;
-      scope.editValue = scope.get();
+      scope.model = {};
 
-      scope.dom = element;
+      scope.$valueGetter = $parse(attrs.value);
+      scope.$valueSetter = scope.$valueGetter.assign;
 
-      scope.enterEditMode = function() {
-        scope.isEditMode = true;
-        $timeout(function() { scope.dom.find('input').focus(); }, 0);
+      scope.getValue = function() {
+        var value = scope.$valueGetter(scope);
+        scope.model.undo = value;
+        scope.model.editValue = value;
+      };
+
+      scope.setValue = function() {
+        var newValue = scope.model.editValue;
+        scope.model.undo = newValue;
+        scope.$valueSetter(scope, newValue);
+      };
+
+      scope.revertValue = function() {
+        scope.model.editValue = scope.model.undo;
+      };
+
+      scope.model.isEditMode = false;
+      scope.getValue();
+
+      scope.enterEditMode = function(event) {
+        scope.model.isEditMode = true;
+        scope.getValue();
+        $timeout(function() { element.find('input').focus(); }, 0);
+      };
+
+      scope.exitEditMode = function(event) {
+        switch (event.which) {
+          case 13:
+            scope.model.isEditMode = false;
+            scope.setValue();
+            break;
+          case 27:
+            scope.model.isEditMode = false;
+            scope.revertValue();
+            break;
+        }
       };
     }
   }; // return
