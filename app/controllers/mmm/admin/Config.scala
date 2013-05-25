30a29a9fc8c4549c80510754ab7f7ffcf6849273
Marat Kh. Akhin <Marat.Akhin@gmail.com>
2013-05-25 23:15:06 +0400

    Funky Config.set implemented

--- app/controllers/mmm/admin/Config.scala
+++ app/controllers/mmm/admin/Config.scala
@@ -1,21 +1,75 @@
 package controllers.mmm.admin
 
-import play.api.libs.json.Json
+import play.api.libs.json.{Json, JsObject, JsError, JsSuccess, JsString}
 import play.api.mvc._
-import util.JsonRestSuccess
+import scala.util.Failure
 import util.MmmJsonifier._
+import util._
 
 object Config
   extends Controller {
 
   import util.config.ConfigManager.{Instance => CMI}
 
-  def get = Action {
-    Ok(
-      JsonRestSuccess(Json.obj(
-        CMI.asMap().map { case (name, value) => (name, Json.toJsFieldJsValueWrapper(value)(TypedAnyWrites)) }.toSeq: _*
-      ))
+  def jsonMap(m: Map[String, Any]): JsObject = {
+    Json.obj(
+      m.map { case (name, value) => (name, Json.toJsFieldJsValueWrapper(value)(TypedAnyWrites)) }.toSeq: _*
     )
   }
 
+  def get = Action {
+    Ok(
+      JsonRestSuccess(jsonMap(CMI.asMap()))
+    )
+  }
+
+  def set = Action(parse.json) {
+    request => {
+
+      request.body
+      .validate[JsObject] match {
+
+        case e: JsError =>
+          BadRequest(
+            JsonRestFailure("JSON object expected")
+          )
+
+        case s: JsSuccess[JsObject] => {
+          s.get
+          .fields
+          .map {
+            case (path, value) =>
+              value \ "value" match {
+                case v: JsString =>
+                  CMI.set(path, v.value)
+                  .flatMap { _ => CMI.get(path) }
+                  .map { v => (path -> v) }
+                case _ =>
+                  Failure(new NoSuchElementException(
+                    s"Value field not found for '$path' in: $value"
+                  ))
+              }
+          }.partition { t => t.isSuccess } match {
+
+            case (ok, Nil) => {
+              Ok(
+                JsonRestSuccess(jsonMap(ok.map { _.get }.toMap))
+              )
+            }
+
+            case (ok, errors) => {
+              UnprocessableEntity(
+                JsonRestFailure(Json.obj(
+                  "msg" -> Json.toJson(errors.head.failed.get),
+                  "changed" -> jsonMap(ok.map { _.get }.toMap)
+                ))
+              )
+            }
+
+          }
+        }
+      }
+
+    }
+  }
 }
