2ac816f01a119a1377b493fcf3d9208c2392089a
Marat Kh. Akhin <Marat.Akhin@gmail.com>
2013-07-24 23:28:11 +0400

    Periodic ConfigManager DB sync

--- app/global/Global.scala
+++ app/global/Global.scala
@@ -3,9 +3,12 @@ package global
 import info.icephoenix.mmm.MegamekManager
 import info.icephoenix.mmm.data._
 import play.api._
+import play.api.libs.concurrent.Akka
+import scala.concurrent.duration._
 
 object Global extends GlobalSettings {
 
+  import play.api.libs.concurrent.Execution.Implicits._
   import util.config.ConfigManager.{Instance => CMI}
 
   var mmm: MegamekManager = _
@@ -18,6 +21,8 @@ object Global extends GlobalSettings {
     CMI.Servers.Ports.foreach {
       port => mmm.RunnerSup ! StartServer(port, "")
     }
+
+    Akka.system(app).scheduler.schedule(1.minutes, 1.minutes) { CMI.save() }
   }
 
   override def onStop(app: Application) {
